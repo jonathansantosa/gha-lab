26f90beb591dbfc8e2836abca29362d4ab02e331
Jonathan Santosa <jonathansantosa@Jonathans-MacBook-Pro-4.local>
2026-05-19 20:38:20 -0700

    Initial app

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from app import add, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+    assert multiply(0, 100) == 0
